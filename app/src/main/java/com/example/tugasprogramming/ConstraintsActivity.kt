f93e3c4b75b9ea5962887878d86517c07fb64d5e
fabinwantara <favinvantara8@gmail.com>
2022-10-25 16:31:42 +0700

    first commit

---
+++ app/src/main/java/com/example/tugasprogramming/ConstraintsActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugasprogramming
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ConstraintsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraints_activity)
+    }
+}
